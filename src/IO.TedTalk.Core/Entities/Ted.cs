a8fa3b56327cb1f435886987d6929f09126008ee
Saeed Salehi <s.salehi@mootanroo.com>
2022-06-14 00:51:16 +0430

    change entity type

--- src/IO.TedTalk.Core/Entities/Ted.cs
+++ src/IO.TedTalk.Core/Entities/Ted.cs
@@ -4,9 +4,11 @@ public class Ted : BaseEntity
 {
     public string Title { get; set; }
     public string Author { get; set; }
-    public DateTime Date { get; set; }
-    public long Views { get; set; }
-    public long Likes { get; set; }
+    public string Date { get; set; }
+    //TODO: I found some strange data (maybe invalid) in csv file , so I've changed type to string! 
+    public string Views { get; set; }
+    //TODO: same as Views property :-?
+    public string Likes { get; set; }
     public string Link { get; set; }
 
 }
